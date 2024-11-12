2b87bac944b8b5a9d4eefd8eef61d22b63d45e49
anubis_greyignis <marceloramalho.info@gmail.com>
2024-11-12 11:45:19 -0300

    Creating subject filter to pass inside the url and fixing bugs and null responses in get_df_emails

--- hermes/hermes_msgraph.py
+++ hermes/hermes_msgraph.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-#https://graph.microsoft.com/v1.0/me/messages?$filter=subject eq '{subject}' and sender/emailAddress/address eq '{sender email address}' and sentDateTime ge 2023-05-17T07:28:08Z
+# https://graph.microsoft.com/v1.0/me/messages?$filter=subject eq '{subject}' and sender/emailAddress/address eq '{sender email address}' and sentDateTime ge 2023-05-17T07:28:08Z
 # coding: utf-8
 import json
 import requests
@@ -8,16 +8,23 @@ import pandas as pd
 
 class MSGraphAPI:
     """
-    Classe para interagir com a API do Microsoft Graph para envio e leitura de e-mails.
+    Class to interact with the Microsoft Graph API for sending and reading emails.
 
-    A classe contém métodos para obter o token de acesso, enviar e-mails, ler mensagens de e-mail, organizar os dados em um DataFrame e salvar em arquivo JSON.
+    The class contains methods to obtain an access token, send emails, read email messages, organize data into a DataFrame, and save it to a JSON file.
     """
 
     def __init__(self, client_id, client_secret, tenant_id):
         """
-        Inicializa a classe com as credenciais passadas como parametro
-        client_id, client_secret, tenant_id
+        Initializes the class with the credentials passed as parameters.
+
+        Parameters:
         ----------
+        client_id : str
+            Client ID registered in Azure AD.
+        client_secret : str
+            Client secret registered in Azure AD.
+        tenant_id : str
+            Directory (tenant) ID in Azure AD.
         """
         self.client_id = client_id
         self.client_secret = client_secret
@@ -25,12 +32,17 @@ class MSGraphAPI:
 
     def __get_access_token(self):
         """
-        Método privado para obter o token de acesso à API do Microsoft Graph.
+        Private method to obtain the access token for the Microsoft Graph API.
 
-        Retorna:
+        Returns:
         -------
         access_token : str
-            Token de acesso para realizar chamadas à API do Microsoft Graph.
+            Access token to make API calls to Microsoft Graph.
+
+        Raises:
+        ------
+        RuntimeError:
+            If the request fails when trying to obtain the access token.
         """
         url = f"https://login.microsoftonline.com/{self.tenant_id}/oauth2/v2.0/token"
         payload = {
@@ -42,7 +54,7 @@ class MSGraphAPI:
         try:
             response = requests.post(url, data=payload)
         except Exception as e:
-            raise RuntimeError("Não foi possível retornar o request do post para pegar o access token") from e
+            raise RuntimeError("Unable to make post request to get access token") from e
         
         response_data = response.json()
         access_token = response_data['access_token']
@@ -50,20 +62,24 @@ class MSGraphAPI:
 
     def send_email(self, sender_mail, subject, body, to_address, cc_address=None):
         """
-        Envia um e-mail usando a API do Microsoft Graph.
+        Sends an email using the Microsoft Graph API.
 
         Parameters:
         ----------
         sender_mail : str
-            E-mail do remetente.
+            Sender's email address.
         subject : str
-            Assunto do e-mail.
+            Email subject.
         body : str
-            Corpo do e-mail.
+            Email body.
         to_address : str
-            Endereço de e-mail do destinatário.
+            Recipient's email address.
         cc_address : str, optional
-            Endereço de e-mail para cópia, por padrão None.
+            CC email address, default is None.
+
+        Returns:
+        -------
+        None
         """
         access_token = self.__get_access_token()
         headers = {
@@ -101,11 +117,29 @@ class MSGraphAPI:
         response = requests.post(url, headers=headers, data=json.dumps(payload))
 
         if response.status_code == 200:
-            print("E-mail enviado com sucesso!")
+            print("Email sent successfully!")
         else:
-            print(f"Erro ao enviar e-mail: {response.status_code}")
+            print(f"Error sending email: {response.status_code}")
 
     def get_email_folders(self, email_address):
+        """
+        Retrieves the email folders for the specified user.
+
+        Parameters:
+        ----------
+        email_address : str
+            User's email address.
+
+        Returns:
+        -------
+        list
+            A list containing the email folders.
+
+        Raises:
+        ------
+        RuntimeError:
+            If an error occurs when accessing the email folders.
+        """
         access_token = self.__get_access_token()
         headers = {
             "Authorization": f"Bearer {access_token}",
@@ -122,9 +156,22 @@ class MSGraphAPI:
             mail_folders = response.json().get('value', [])
             return mail_folders
         else:
-            print(f"Erro ao ler e-mails: {response.status_code}")
+            print(f"Error reading emails: {response.status_code}")
 
     def list_email_folders(self, email_address):
+        """
+        Lists email folders and returns a DataFrame.
+
+        Parameters:
+        ----------
+        email_address : str
+            User's email address.
+
+        Returns:
+        -------
+        pandas.DataFrame
+            DataFrame containing the email folders.
+        """
         mail_folders = self.get_email_folders(email_address)
         df_mail_folders = pd.DataFrame(mail_folders)
         if not df_mail_folders.empty:
@@ -132,18 +179,29 @@ class MSGraphAPI:
 
     def __read_email(self, email_address, subject, folder, sender, n_of_massages, subject_filter, messages_json_path):
         """
-        Método privado para ler e-mails de um endereço específico.
+        Private method to read emails from a specific address.
 
         Parameters:
         ----------
-        messages_json_path : str
-            Caminho do arquivo JSON onde as mensagens serão salvas.
         email_address : str
-            Endereço de e-mail do qual ler as mensagens.
+            Email address to read messages from.
+        subject : str
+            Subject to filter.
+        folder : str
+            Folder name to filter messages.
+        sender : str
+            Sender's address.
         n_of_massages : int
-            Número de mensagens a serem recuperadas.
-        """
+            Number of messages to retrieve.
+        subject_filter : str
+            Subject filter to apply to messages.
+        messages_json_path : str
+            Path to the JSON file where messages will be saved.
 
+        Returns:
+        -------
+        None
+        """
         email_filter = []
         email_filter_url = []
 
@@ -154,11 +212,14 @@ class MSGraphAPI:
         
         if sender:
             email_filter.append(f"sender/emailAddress/address eq '{sender}'")
+        
+        if subject:
+            email_filter.append(f"subject eq '{subject}'")
 
         if email_filter:
             email_filter_joined = " and ".join(email_filter)
             email_filter_url.append(f"$filter={email_filter_joined}")
-        n_of_massages = ''
+
         if n_of_massages:
             email_filter_url.append(f"$top={n_of_massages}")
                 
@@ -167,12 +228,12 @@ class MSGraphAPI:
             "Authorization": f"Bearer {access_token}",
             "Content-Type": "application/json"
         }
-        
-
-        email_filter_url = "&".join(email_filter_url)
+        if email_filter_url:
+            email_filter_url = "&".join(email_filter_url)
+        else:
+            email_filter_url = ''
 
         url = f"https://graph.microsoft.com/v1.0/users/{email_address}{folder}/messages?{email_filter_url}"
-        print(url)
         response = requests.get(url, headers=headers)
         
         if response.status_code == 200:
@@ -181,23 +242,23 @@ class MSGraphAPI:
                 with open(messages_json_path, 'w+', encoding='utf-8') as file:
                     json.dump(messages, file, ensure_ascii=False, indent=4)
             except Exception as e:
-                raise RuntimeError("Não foi possível salvar as mensagens no arquivo messages.json") from e
+                raise RuntimeError("Unable to save messages to messages.json file") from e
         else:
-            print(f"Erro ao ler e-mails: {response.status_code}")
+            print(f"Error reading emails: {response.status_code}")
 
     def __json_to_dataframe(self, json_file_path):
         """
-        Método privado para converter um arquivo JSON de mensagens de e-mail em um DataFrame.
+        Private method to convert a JSON file of email messages into a DataFrame.
 
         Parameters:
         ----------
         json_file_path : str
-            Caminho do arquivo JSON com as mensagens de e-mail.
+            Path to the JSON file with email messages.
 
         Returns:
         -------
         df_emails : pandas.DataFrame
-            DataFrame contendo as mensagens de e-mail.
+            DataFrame containing email messages.
         """
         with open(json_file_path, 'r') as file:
             json_data = json.load(file)
@@ -206,17 +267,17 @@ class MSGraphAPI:
 
     def __organize_df_emails(self, df_emails):
         """
-        Método privado para organizar o DataFrame de mensagens de e-mail com colunas relevantes.
+        Private method to organize the email messages DataFrame with relevant columns.
 
         Parameters:
         ----------
         df_emails : pandas.DataFrame
-            DataFrame com as mensagens de e-mail.
+            DataFrame with email messages.
 
         Returns:
         -------
         df_emails : pandas.DataFrame
-            DataFrame organizado com colunas específicas.
+            Organized DataFrame with specific columns.
         """
         columns = ['subject', 'isRead', 'sentDateTime', 'receivedDateTime', 'sender.emailAddress.name', 
                    'sender.emailAddress.address', 'from.emailAddress.name', 'from.emailAddress.address', 
@@ -224,8 +285,22 @@ class MSGraphAPI:
         df_emails = df_emails[columns]
         return df_emails
     
-    def __filter_subject(self, df, subject_filter): #maybe use rejex
+    def __filter_subject(self, df, subject_filter):
+        """
+        Filters the DataFrame for email messages matching the subject filter.
 
+        Parameters:
+        ----------
+        df : pandas.DataFrame
+            DataFrame of email messages.
+        subject_filter : str
+            Subject filter that may use asterisks for partial matching.
+
+        Returns:
+        -------
+        df : pandas.DataFrame
+            DataFrame filtered by subject.
+        """
         if subject_filter:
             # Case: starts and ends with asterisk -> "contains"
             if subject_filter.startswith("*") and subject_filter.endswith("*"):
@@ -250,53 +325,73 @@ class MSGraphAPI:
 
     def get_df_emails(self, email_address, subject='', folder='', sender='', n_of_massages=10, subject_filter="", messages_json_path='messages.json'):
         """
-        Método público para obter as mensagens de e-mail e organizá-las em um DataFrame apenas com colunas normalmente necessárias, sem todos os atributos do e-mail.
+        Public method to retrieve email messages and organize them into a DataFrame with only essential columns, excluding all email attributes.
 
         Parameters:
         ----------
         email_address : str
-            Endereço de e-mail do qual obter as mensagens.
-        messages_json_path : str, optional
-            Caminho do arquivo JSON onde os e-mails serão salvas, por padrão 'messages.json'.
+            Email address from which to retrieve messages.
+        subject : str, optional
+            Subject to filter by, default is an empty string.
+        folder : str, optional
+            Folder to retrieve messages from, default is an empty string.
+        sender : str, optional
+            Sender's email address to filter by, default is an empty string.
         n_of_massages : int, optional
-            Número de mensagens a coletar, por padrão 10.
+            Number of messages to retrieve, default is 10.
+        subject_filter : str, optional
+            Filter to apply to email subjects, default is an empty string.
+        messages_json_path : str, optional
+            Path to the JSON file where emails will be saved, default is 'messages.json'.
 
         Returns:
         -------
         df_emails : pandas.DataFrame
-            DataFrame organizado com as mensagens de e-mail.
+            Organized DataFrame with email messages.
         """
         self.__read_email(email_address, subject, folder, sender, n_of_massages, subject_filter, messages_json_path)
         df_raw_emails = self.__json_to_dataframe(messages_json_path)
-        df_emails = self.__organize_df_emails(df_raw_emails)
+        if not df_raw_emails.empty:
+            df_emails = self.__organize_df_emails(df_raw_emails)
+        else:
+            df_emails = ''
+            print("No emails found using the parameters passed.")
+            pass
         if subject_filter != "":
             df_emails = self.__filter_subject(df_emails, subject_filter)
         return df_emails
-    # Exemplo de uso:
+    # Usage example:
     # api = MSGraphAPI()
-    # df_emails = api.get_df_emails("anakin.skywalker@fitenergia.com.br")
+    # df_emails = api.get_df_emails("anakin.skywalker@github.com")
 
     def get_raw_df_emails(self, email_address, subject='', folder='', sender='', n_of_massages=10, subject_filter="", messages_json_path='messages.json'):
         """
-        Método público para obter as mensagens de e-mail e organizá-las em um DataFrame com todos os atributos recebidos.
+        Public method to retrieve email messages and organize them into a DataFrame with all received attributes.
 
         Parameters:
         ----------
         email_address : str
-            Endereço de e-mail do qual obter as mensagens.
-        messages_json_path : str, optional
-            Caminho do arquivo JSON onde as mensagens serão salvas, por padrão 'messages.json'.
+            Email address from which to retrieve messages.
+        subject : str, optional
+            Subject to filter by, default is an empty string.
+        folder : str, optional
+            Folder to retrieve messages from, default is an empty string.
+        sender : str, optional
+            Sender's email address to filter by, default is an empty string.
         n_of_massages : int, optional
-            Número de e-mails que deseja ver, por padrão 10.
+            Number of emails to retrieve, default is 10.
+        subject_filter : str, optional
+            Filter to apply to email subjects, default is an empty string.
+        messages_json_path : str, optional
+            Path to the JSON file where messages will be saved, default is 'messages.json'.
 
         Returns:
         -------
         df_emails : pandas.DataFrame
-            DataFrame completo com as mensagens de e-mail e todos os atributos.
+            Full DataFrame with email messages and all attributes.
         """
         self.__read_email(email_address, subject, folder, sender, n_of_massages, subject_filter, messages_json_path)
         df_raw_emails = self.__json_to_dataframe(messages_json_path)
         if subject_filter != "":
             df_raw_emails = self.__filter_subject(df_raw_emails, subject_filter)
         return df_raw_emails
-    
